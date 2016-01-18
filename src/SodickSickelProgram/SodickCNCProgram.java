1e009d835911bcd806135b07f7320994788ee322
mats.andersson@mecona.se <>
2016-01-18 16:25:14 +0100

    Påbörjat arbete för att bygga huvud och underprogrammen

---
+++ src/SodickSickelProgram/SodickCNCProgram.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package SodickSickelProgram;
+
+import Toolpkg.Chain;
+
+/**
+ *
+ * @author Mats
+ */
+public class SodickCNCProgram {
+    private final MainProgram mainProgram;
+    private final CutProgram cutProgram;
+    private final PartProgram partProgram;
+    private final FirstReliefProgram firstReliefProgram;
+    private final SecondReliefProgram secondReliefProgram;
+    
+
+    public SodickCNCProgram() {
+        mainProgram = new MainProgram();
+        cutProgram = new CutProgram();
+        partProgram = new PartProgram();
+        firstReliefProgram = new FirstReliefProgram();
+        secondReliefProgram = new SecondReliefProgram();
+    }
+
+    public void setChains(Chain partChain, Chain cutGeoChain, Chain firstReleifChain, Chain secondReliefChain) {
+        partProgram.setChain(partChain);
+        cutProgram.setChain(cutGeoChain);
+        firstReliefProgram.setChain(firstReleifChain);
+        secondReliefProgram.setChain(secondReliefChain);
+        
+        mainProgram.setPartStartEnd( partChain );
+    }
+    
+    public void buildProgram() {
+    
+    }   
+    
+       
+}
