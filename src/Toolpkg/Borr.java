b1df07122e25db53c5c36b712cade6d283f27ae7
Meconer <mats.asson@gmail.com>
2016-03-02 22:28:25 +0100

    Omstrukturerat calculate och delat upp den i flera metoder för att få beräkningarna lite mer strukturerade.

--- src/Toolpkg/Borr.java
+++ src/Toolpkg/Borr.java
@@ -6,6 +6,8 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import javafx.beans.property.DoubleProperty;
 import javafx.beans.property.SimpleDoubleProperty;
+import org.apache.commons.math3.exception.MathArithmeticException;
+import org.apache.commons.math3.exception.MathIllegalArgumentException;
 import org.apache.commons.math3.geometry.euclidean.threed.Line;
 import org.apache.commons.math3.geometry.euclidean.threed.Plane;
 import org.apache.commons.math3.geometry.euclidean.threed.Rotation;
@@ -42,9 +44,6 @@ public class Borr {
         }
     }
     
-    private final ArrayList<XRadiusAngleTriplet> drillList = new ArrayList<>();
-    private final ArrayList<Plane> planeList = new ArrayList<>();
-
     private final DoubleProperty tipThickness;   
     private final DoubleProperty angle;
     private final DoubleProperty stockDiameter;
@@ -75,7 +74,8 @@ public class Borr {
     }
 
 
-    private void buildTripletListFromSpecText() {
+    private ArrayList<XRadiusAngleTriplet> buildTripletListFromSpecText() {
+        
         String[] specList = specText.split("\n");
         String doubleRegex = "[-+]?[0-9]*\\.?[0-9]*";
         String zRegex = "[zZ](" + doubleRegex + ")";
@@ -85,6 +85,10 @@ public class Borr {
         Pattern radiusPattern = Pattern.compile(radiusRegex);
         Pattern anglePattern = Pattern.compile(angleRegex);
         
+        
+        ArrayList<XRadiusAngleTriplet> drillList = new ArrayList<>();
+
+
         double radius = 0;
         double z = 0;
         double ang = 0;
@@ -98,6 +102,7 @@ public class Borr {
             if ( radiusMatcher.find() ) radius = Double.parseDouble(radiusMatcher.group(1));
             drillList.add( new XRadiusAngleTriplet(z, radius, ang));
         }
+        return drillList;
     }
 
     
@@ -117,51 +122,14 @@ public class Borr {
     // Calculate the chains for the drill cutting toolpath.
     public void calculate() {
         
-        buildTripletListFromSpecText();
-        
-        XRadiusAngleTriplet prevTriplet;
-        XRadiusAngleTriplet nextTriplet;
+        // Calculate a list of z, angle and radius from the spec text.
+        ArrayList<XRadiusAngleTriplet> drillList = buildTripletListFromSpecText();
 
-        // Calculate a series of planes that make up the drill edges.
-        Iterator<XRadiusAngleTriplet> tripletIterator = drillList.iterator();
-        if ( !tripletIterator.hasNext() ) throw new IndexOutOfBoundsException("borrList har inga element");
-        prevTriplet = tripletIterator.next();
-        while ( tripletIterator.hasNext() ) {
-            
-            // Calculate the first 3D point on this part of the drill
-            double xPrev = prevTriplet.getX();
-            double zPrev = halfBlankThickness(xPrev);
-            double yPrev = calculateY( prevTriplet.getRadius(), zPrev);
-            Vector3D startPoint = new Vector3D( xPrev, yPrev, zPrev);
+        // Use the above list to construct a list of Planes that make up the drill
+        // edges.
+        ArrayList<Plane> planeList = calcPlaneList(drillList);
         
-            // The same for the second point
-            nextTriplet = tripletIterator.next();
-            double xNext = nextTriplet.getX();
-            double zNext = halfBlankThickness(xNext);
-            double yNext = calculateY(nextTriplet.getRadius(), zNext);
-            Vector3D nextPoint = new Vector3D(xNext, yNext, zNext);
-            
-            // Build a vertical plane through the two calculated points.
-            // First a vector between the points.
-            Vector3D diffVector = nextPoint.subtract(startPoint);
-            
-            // The normal vector of the vertical plane through these two points
-            // The z part is 0 for a vertical plane
-            Vector3D normalVector = new Vector3D( diffVector.getY(), -diffVector.getX(),0);
-            // Make the plane
-            Plane verticalPlane = new Plane(startPoint, normalVector, Constants.TOLERANCE);
-            
-            // Now rotate the plane around the diff vector
-            // The plane will still go through both start and end points but not vertical.
-            Rotation rotation = new Rotation(diffVector, Math.toRadians(nextTriplet.getAngle()), RotationConvention.VECTOR_OPERATOR);
-            Plane nextPlane = verticalPlane.rotate(startPoint, rotation);
-            planeList.add( nextPlane);
-            
-            // Use the nextPoint as startPoint for the next loop.
-            prevTriplet = nextTriplet;
-        }
         
-        // We now have the planes that make up the drill edges. 
         // Now we will calculate the intersection lines between all consecutive planes.
         // For the first intersection we will use the first plane but rotated 180 degrees
         // around the x axis.
@@ -190,18 +158,68 @@ public class Borr {
         Plane belowPlane = new Plane( new Vector3D(0,0,-2), new Vector3D( 0,0,1), Constants.TOLERANCE);
         
         // Now make two lists of intersection points with the lines in the line lists
-        ArrayList<Vector3D> abovePointList = new ArrayList<>();
-        ArrayList<Vector3D> belowPointList = new ArrayList<>();
+        ArrayList<Vector3D> abovePointList = buildPointList(lineList, abovePlane, false);
+        ArrayList<Vector3D> belowPointList = buildPointList( lineList, belowPlane, true );
 
+        
+    }
+
+    private ArrayList<Vector3D> buildPointList(ArrayList<Line> lineList, Plane programmingPlane, Boolean mirror) {
+        ArrayList<Vector3D> pointList = new ArrayList<>();
         for ( Line line : lineList ) {
-            Vector3D aboveIntersection = abovePlane.intersection(line);
-            Vector3D belowIntersection = belowPlane.intersection(line);
-            Vector3D mirroredBelow = new Vector3D(belowIntersection.getX(), -belowIntersection.getY(), -belowIntersection.getZ());
-            abovePointList.add(aboveIntersection);
-            belowPointList.add(mirroredBelow);
-            System.out.println("Point : " + aboveIntersection.toString());
-            System.out.println("BelowPoint : " + mirroredBelow.toString());
+            Vector3D intersection = programmingPlane.intersection(line);
+            if ( mirror ) intersection = new Vector3D( intersection.getX(), -intersection.getY(), -intersection.getZ());
+            pointList.add(intersection);
+            if ( mirror ) System.out.println("BelowPoint : " + intersection.toString());
+            else System.out.println("Point : " + intersection.toString());
         }
+        return pointList;
+    }
+
+    
+    // Calculate a series of planes that make up the drill edges.
+    private ArrayList<Plane> calcPlaneList(ArrayList<XRadiusAngleTriplet> drillList) throws IndexOutOfBoundsException, MathArithmeticException, MathIllegalArgumentException {
+        XRadiusAngleTriplet nextTriplet;
+
+        ArrayList<Plane> planeList = new ArrayList<>();
+        Iterator<XRadiusAngleTriplet> tripletIterator = drillList.iterator();
+        if ( !tripletIterator.hasNext() ) throw new IndexOutOfBoundsException("borrList har inga element");
+        XRadiusAngleTriplet prevTriplet = tripletIterator.next();
+        while ( tripletIterator.hasNext() ) {
+            
+            // Calculate the first 3D point on this part of the drill
+            double xPrev = prevTriplet.getX();
+            double zPrev = halfBlankThickness(xPrev);
+            double yPrev = calculateY( prevTriplet.getRadius(), zPrev);
+            Vector3D startPoint = new Vector3D( xPrev, yPrev, zPrev);
+            
+            // The same for the second point
+            nextTriplet = tripletIterator.next();
+            double xNext = nextTriplet.getX();
+            double zNext = halfBlankThickness(xNext);
+            double yNext = calculateY(nextTriplet.getRadius(), zNext);
+            Vector3D nextPoint = new Vector3D(xNext, yNext, zNext);
+            
+            // Build a vertical plane through the two calculated points.
+            // First a vector between the points.
+            Vector3D diffVector = nextPoint.subtract(startPoint);
+            
+            // The normal vector of the vertical plane through these two points
+            // The z part is 0 for a vertical plane
+            Vector3D normalVector = new Vector3D( diffVector.getY(), -diffVector.getX(),0);
+            // Make the plane
+            Plane verticalPlane = new Plane(startPoint, normalVector, Constants.TOLERANCE);
+            
+            // Now rotate the plane around the diff vector
+            // The plane will still go through both start and end points but not vertical.
+            Rotation rotation = new Rotation(diffVector, Math.toRadians(nextTriplet.getAngle()), RotationConvention.VECTOR_OPERATOR);
+            Plane nextPlane = verticalPlane.rotate(startPoint, rotation);
+            planeList.add( nextPlane);
+            
+            // Use the nextPoint as startPoint for the next loop.
+            prevTriplet = nextTriplet;
+        }
+        return planeList;
     }
     
 }
